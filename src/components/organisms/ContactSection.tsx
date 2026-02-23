534cfc6f6b58dd730ac5abd0f4d5a4e0dca7bebf
h2m_kinoko <happy2mylife@gmail.com>
2026-02-24 08:08:32 +0900

    youtube icon

--- src/components/organisms/ContactSection.tsx
+++ src/components/organisms/ContactSection.tsx
@@ -24,6 +24,15 @@ const ContactSection: React.FC = () => {
           <SocialLink href="https://twitter.com/h2m_kinoko" icon={<Twitter size={24} />} title="Twitter" />
           <SocialLink href="https://www.facebook.com/happy.happy.children/" icon={<Facebook size={24} />} title="Facebook" />
           <SocialLink href="https://lin.ee/RjpJefL" icon={<span className="text-xl font-bold" style={{ color: '#06C755' }}>L</span>} title="LINE" />
+          <SocialLink
+            href="https://www.youtube.com/@mashandroom"
+            icon={
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+                <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z" />
+              </svg>
+            }
+            title="YouTube"
+          />
           <SocialLink href="https://qiita.com/h2m_kinoko" icon="Q" title="Qiita" />
           <SocialLink href="https://note.com/mashandroom" icon="N" title="Note" />
           <SocialLink href="https://8card.net/virtual_cards/kDI3_qr5XPcxOj1JdnGKKQ_30247218107" icon="8" title="Eight (Business Card)" />
